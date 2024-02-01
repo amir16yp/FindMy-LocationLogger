df79c1a4260492931a4b34368aa11763735c504c
Amir Y. Perehodnik <myrprhwdnyq@gmail.com>
2024-02-01 14:22:57 +0200

    Update constants.py

--- find-my-thing/lib/constants.py
+++ find-my-thing/lib/constants.py
@@ -1,7 +1,7 @@
 JSON_LAYER_SEPARATOR = '|'
 FINDMY_FILES = [
     '/private/var/mobile/Library/Caches/com.apple.findmy.fmipcore/Items.data',
-    '/private/var/mobile/Library/Caches/com.apple.findmy.fmipcoreDevices.data']
+    '/private/var/mobile/Library/Caches/com.apple.findmy.fmipcore/Devices.data']
 NAME_SEPARATOR = '_'
 NULL_STR = 'NULL'
 TIME_FORMAT = '%m/%d/%Y, %H:%M:%S'
